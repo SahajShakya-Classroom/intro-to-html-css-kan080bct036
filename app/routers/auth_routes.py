cc85c92afb9ee617f70863b5d41062e13260ad70
kan080bct036 <kan080bct036@kec.edu.np>
2026-03-28 17:23:35 +0545

    lab5

---
+++ app/routers/auth_routes.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from .. import schemas, crud, database, auth
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["auth"]
+)
+
+@router.post("/register", response_model=schemas.UserOut)
+def register(user: schemas.UserCreate, db: Session = Depends(database.get_db)):
+    db_user = crud.get_user_by_email(db, user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db, user)
+
+@router.post("/login")
+def login(user: schemas.UserLogin, db: Session = Depends(database.get_db)):
+    db_user = crud.get_user_by_email(db, user.email)
+    if not db_user or not crud.verify_password(user.password, db_user.hashed_password):
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+    token = auth.create_access_token({"sub": db_user.email})
+    return {"access_token": token, "token_type": "bearer"}
